2bcd3667e371d7c396c2a46a4c4f835a0f417027
Serveriii <sjokelainen@outlook.com>
2025-01-26 17:02:23 +0200

    new project created, initial commit

--- first_ap/android/app/src/main/kotlin/com/example/first_ap/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.first_ap
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ first_app/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+        ),
+        body: const Text('Hello wrolds')),
+  ));
+}
