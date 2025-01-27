e642497b30c980845927374a8558f95af3ad30df
Serveriii <sjokelainen@outlook.com>
2025-01-27 15:25:11 +0200

    jups

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -3,9 +3,24 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color.fromARGB(255, 72, 12, 236),
+              Color.fromARGB(255, 117, 5, 209)
+            ],
+          ),
         ),
-        body: const Text('Hello wrolds')),
+        child: Center(
+          child: Text(
+            'Some text.',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    ),
   ));
 }
